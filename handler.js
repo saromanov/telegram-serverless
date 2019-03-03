a765a5d51bf09402fa4b7ad53f290ca06b9b47da
Sergey <xxsmotur@gmail.com>
2019-03-03 18:42:51 +0500

    draft version

--- handler.js
+++ handler.js
@@ -1,7 +1,15 @@
 'use strict';
 
-const { recv } = require('./telegram/telegram');
+const { recv, response } = require('./telegram/telegram');
 
 module.exports.run = (event, context, callback) => {
-  
+      recv(event)
+        .then((response) => {
+          console.log(JSON.stringify(response));
+          return callback(null, { statusCode: json.status, body: JSON.stringify({ message: response }) });
+        })
+        .catch((error) => {
+          console.error(error);
+          return callback(null, { statusCode: 500, body: JSON.stringify({ error }) });
+        });
 };
